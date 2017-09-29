a6c4e9b9a503a02b45758a18e8338ce61f71a5d9
Mrinalini Sugosh <mrinalini24@berkeley.edu>
2017-09-29 21:55:55 +0000

    hw4-part2 passes tests FINALLY

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -18,7 +18,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  reg_exp = /#{e1}.*{e2}/m
+  reg_exp.match(page.body)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -29,10 +30,17 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(', ').each do |rating|
+    if !uncheck.nil?
+      uncheck("ratings_#{rating}")
+    else
+      check("ratings_#{rating}")
+    end
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  rows = page.all('table#movies tbody tr').count    #used stack overflow for learning how to count rows in a table
+  expect(rows).to eq Movie.count
 end
